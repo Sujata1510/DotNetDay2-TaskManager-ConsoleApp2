39906f5defe5123afb3c91cacf01f8355a286154
Sujata Kutuwa <skutuwa@infinite.com>
2026-08-27 10:20:58 +0545

    Day 1-Assignment

---
+++ day_1.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
+String Name = "Sujata";
+Console.WriteLine(Name);
+
+int Age = 24;
+
+char grade = 'A';
+
+double price = 99.99;
+
+bool isActive = true;
+
